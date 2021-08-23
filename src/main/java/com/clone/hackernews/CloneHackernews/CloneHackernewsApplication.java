ceaa0c90522766c61ad35c006e4ea35a80e1907d
Rutvik2901 <gajjarrutvik291@gmail.com>
2021-08-23 16:51:46 +0530

    Intial Commit

---
+++ src/main/java/com/clone/hackernews/CloneHackernews/CloneHackernewsApplication.java
@@ -0,0 +1,13 @@
+package com.clone.hackernews.CloneHackernews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneHackernewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneHackernewsApplication.class, args);
+	}
+
+}
